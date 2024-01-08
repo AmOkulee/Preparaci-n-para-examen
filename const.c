84e866dd5c3c4555cd5cc05ff29e2161952299ad
ALE <141135202+AmOkulee@users.noreply.github.com>
2024-01-08 18:42:19 +0100

    Create const.c
    demostración de const en c

---
+++ const.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+  const double PI = 3.141592;
+  int main(){
+    double area;
+    double radio = 12.45;
+    area = PI*(radio * radio);
+    printf("el area es %lf : ", area);	
+return 0;}

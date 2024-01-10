1e6f401a6642797e5b71261766d5ac9f26de4544
ALE <141135202+AmOkulee@users.noreply.github.com>
2024-01-10 22:46:43 +0100

    Update recorrer una matriz y verificar elementoc
    recorrer una matriz y verificar el elemento

--- recorrer una matriz y sumar promedio.c
+++ recorrer una matriz y sumar promedio.c
@@ -1,1 +1,34 @@
+#include<stdio.h>
 
+int main(){
+int num;
+  int matriz[4][4]={{1,2,3,4},
+                    {5,6,7,8},
+                   {9,0,11,22},
+                  {33,44,55,66}};
+  
+printf("digite un numero para buscar en la matriz: \n");
+scanf("%d", num);
+  
+  int i,j;
+  int f = -1;
+  int c = -1;
+  for(i=0;i<4;i++){
+    for(j=0;j<4;j++){
+      if(matriz[i][j] == num){
+        break;
+        f = i+1;
+        c = j+1;
+      }
+    }if(c! =-1){
+      break;
+    }
+  }
+  if(c == -1 && f == -1){
+
+    printf("su numero no fue encontrado \n" );
+  }else {
+printf("el dato se encuentra en la fila %d, y en la columna %d \n", f, c);
+    
+  }
+return 0;}
